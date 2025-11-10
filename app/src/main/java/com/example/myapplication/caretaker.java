778fab60bada40d88466b537a553a18fcd79d5dd
Allin Ramjit <allinramjit@proton.me>
2025-11-10 13:38:09 -0400

    Created medication and caretaker classes

---
+++ app/src/main/java/com/example/myapplication/caretaker.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+public class caretaker {
+    private String name;
+    private int phoneNum;
+
+
+    public String getName(){return this.name;}
+
+    public int getPhoneNum(){return this.phoneNum;}
+
+    public void setName(String name){this.name = name;}
+
+    public void setPhoneNum(int num){this.phoneNum = num;}
+}
---
+++ app/src/main/java/com/example/myapplication/medication.java
@@ -0,0 +1,12 @@
+package com.example.myapplication;
+
+public class medication {
+    private String name;
+    private int dosage;
+
+    public String getName(){return this.name;}
+    public int getDosage(){return this.dosage;}
+    public void setName(String name){this. name = name;}
+    public void setDosage(int dosage){tihs.dosage = dosage;}
+
+}
